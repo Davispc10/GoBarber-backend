4b9837825297fdba388e0d566a89fd4ad5f5fe1d
davispc10 <davispc100@gmail.com>
2019-10-14 18:23:13 -0300

    Adição de segurança na api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
+import cors from 'cors';
+
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 
@@ -24,11 +30,32 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(helmet());
+    this.server.use(
+      cors({
+        origin: false, // caminho do front end que poderão acessar o servidor.
+      })
+    );
     this.server.use(express.json());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 10, // 100
+        })
+      );
+    }
   }
 
   routes() {
